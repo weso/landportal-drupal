f4d34fa7a6fdef2cb7f5769b3a1e0f7a92be52ac
Guillermo Infante Hernández <guillermo2104@gmail.com>
2014-01-16 19:49:18 +0100

    Remove deprecated script

--- update-code.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-cd /var/www/portal
-#drush archive-dump --destination=/vagrant/backups/portal.tar.gz
-drush archive-restore /vagrant/landportal-drupal/portal.tar.gz --destination=/var/www/portal/ --overwrite
-
