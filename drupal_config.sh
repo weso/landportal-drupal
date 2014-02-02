64766960ed1f2d5cc7e985cec75b62e9c31df594
belaustegui <cristian.alvb@gmail.com>
2014-02-02 14:14:07 +0100

    Updated Drupal configuration script.

--- drupal_config.sh
+++ drupal_config.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-
 # Enable the following modules
 drush en -y features
 drush en -y logintoboggan
 
-# Download the following modules
+# Dowload the following modules
 drush dl -y oauth
 
 # Enable the custom features
-drush en user_account_fields -y
-drush en facebook_login -y
+drush en -y user_account_fields
+drush en -y facebook_login
+
