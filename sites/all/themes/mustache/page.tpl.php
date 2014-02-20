00b155e5f3d28955c39af27af679d84e12199195
belaustegui <cristian.alvb@gmail.com>
2014-02-20 12:34:59 +0100

    Created 'mustache' theme to render custom pages. See #18

--- sites/all/themes/mustache/page.tpl.php
+++ sites/all/themes/mustache/page.tpl.php
@@ -1,4 +1,5 @@
 <?php
+/*
 	// Mustache loader
 	$mustache_path = libraries_get_path('Mustache') . '/Autoloader.php';
 	require $mustache_path;
@@ -6,7 +7,7 @@
 
 	// Create Mustache engine
 	$mustache = new Mustache_Engine(array(
-   		'loader' => new Mustache_Loader_FilesystemLoader(drupal_get_path('theme', 'custom') . '/views')
+   		'loader' => new Mustache_Loader_FilesystemLoader(drupal_get_path('theme', 'mustache') . '/views')
 	));
 
 	// Read data from JSON file
@@ -15,6 +16,8 @@
 	$template = $mustache->loadTemplate($mustache_template);
 	
 	echo $template->render($data);
+	*/
 
+print 'hola';
 	
 	
---
+++ sites/all/themes/mustache/template.php
@@ -0,0 +1,17 @@
+<?php
+
+/*	
+function custom_preprocess_page(&$vars) {
+	$vars['mustache_template'] = 'index';
+	$vars['mustache_data'] = array("planet" => "World!");
+}
+
+function mustache_theme() {
+	return array(
+		'custom' => array(
+			'variables' => array('salida' => NULL),
+			'template' => 'custom',
+		),
+	);
+}
+*/
