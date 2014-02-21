e4d31729e5b72b281de8d45cf9a8a01347dd0520
belaustegui <cristian.alvb@gmail.com>
2014-02-21 13:16:22 +0100

    Create name-convention MVC for rendering custom templates with Mustache. Closes #18

---
+++ sites/all/modules/custom/modules/landportal_uris/model/countries.php
@@ -0,0 +1,8 @@
+<?php
+
+class Countries {
+	
+	public function get($params) {
+		return array();
+	}
+}
--- sites/all/themes/mustache/page.tpl.php
+++ sites/all/themes/mustache/page.tpl.php
@@ -1,23 +1,18 @@
 <?php
-/*
-	// Mustache loader
-	$mustache_path = libraries_get_path('Mustache') . '/Autoloader.php';
-	require $mustache_path;
-	Mustache_Autoloader::register();
 
-	// Create Mustache engine
-	$mustache = new Mustache_Engine(array(
-   		'loader' => new Mustache_Loader_FilesystemLoader(drupal_get_path('theme', 'mustache') . '/views')
-	));
+// Mustache loader
+$mustache_path = libraries_get_path('Mustache') . '/Autoloader.php';
+require $mustache_path;
+Mustache_Autoloader::register();
 
-	// Read data from JSON file
-	$data = $mustache_data;
-	// Mundo view template
-	$template = $mustache->loadTemplate($mustache_template);
-	
-	echo $template->render($data);
-	*/
+// Create Mustache engine
+$mustache = new Mustache_Engine(array(
+		'loader' => new Mustache_Loader_FilesystemLoader(drupal_get_path('theme', 'mustache') . '/views')
+));
 
-print 'hola';
-	
-	
+// Read data from JSON file
+$data = $mustache_data;
+// Mundo view template
+$template = $mustache->loadTemplate($mustache_template);
+
+echo $template->render($data);
