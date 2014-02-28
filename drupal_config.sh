2980b9fdbe7295550ad5eeb799943af3a0987eee
belaustegui <cristian.alvb@gmail.com>
2014-02-28 19:31:30 +0100

    Update Drupal configuration script to install the module 'Organic Groups'

--- drupal_config.sh
+++ drupal_config.sh
@@ -1,26 +1,29 @@
 #!/usr/bin/env bash
 # Enable the following contrib modules
-drush dl -y logintoboggan
-drush en -y logintoboggan
-drush dl -y libraries
-drush en -y libraries
+	drush dl -y logintoboggan
+	drush en -y logintoboggan
+	drush dl -y libraries
+	drush en -y libraries
 
 # Dowload the following contrib modules
-drush dl -y oauth
+	drush dl -y oauth
 
 # Enable the custom features
-drush en -y user_account_fields
-drush en -y facebook_login
-drush en -y twitter_login
+	drush en -y user_account_fields
+	drush en -y facebook_login
+	drush en -y twitter_login
 
 # Enable the Landportal CKAN Integration modules
-drush en -y ckan_integration_endpoint
-drush en -y druser_resource
-drush en -y session_resource
+	drush en -y ckan_integration_endpoint
+	drush en -y druser_resource
+	drush en -y session_resource
 
 # Enable the Landportal UI modules
-drush en -y landportal_uris
+	drush en -y landportal_uris
 
 # Enable the Landportal Testing modules
-drush en -y unit_testing
+	drush en -y unit_testing
 
+# Enable the LandDebate modules
+	drush dl -y og
+	drush en -y og og_ui
