672777b2766c33a905eac90a3574f08570fdad9a
belaustegui <cristian.alvb@gmail.com>
2014-03-19 11:08:44 +0100

    Update /catalog view. See #29

--- sites/all/modules/custom/modules/landportal_uris/model/catalog.php
+++ sites/all/modules/custom/modules/landportal_uris/model/catalog.php
@@ -3,8 +3,6 @@
 class Catalog {
 	
 	public function get() {
-		$ckan_view = file_get_contents('http://156.35.82.103:1100/data');
-		$data = array('ckan' => $ckan_view);
-        return $data;
+		return "";
 	}
 }
