1196f3c6b92923a42b38e509e244b4430b246f08
belaustegui <cristian.alvb@gmail.com>
2014-03-19 10:41:04 +0100

    Update CKAN URL. See #29

--- sites/all/modules/custom/modules/landportal_uris/model/catalog.php
+++ sites/all/modules/custom/modules/landportal_uris/model/catalog.php
@@ -3,7 +3,7 @@
 class Catalog {
 	
 	public function get() {
-		$ckan_view = file_get_contents('http://156.35.82.103/data');
+		$ckan_view = file_get_contents('http://156.35.82.103:1100/data');
 		$data = array('ckan' => $ckan_view);
         return $data;
 	}
