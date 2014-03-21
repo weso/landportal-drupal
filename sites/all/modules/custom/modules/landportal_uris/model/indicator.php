d4d3bcc9cc648bc5c8670b1da43a733ae46b51c6
belaustegui <cristian.alvb@gmail.com>
2014-03-21 10:38:16 +0100

    Pass entity-id to the views. Closes #31

--- sites/all/modules/custom/modules/landportal_uris/model/indicator.php
+++ sites/all/modules/custom/modules/landportal_uris/model/indicator.php
@@ -4,12 +4,13 @@ class Indicator {
 	
 	public function get($indicator_id) {
         $temporal_indicator = 'indicator';
-        $module_path = drupal_get_path("module", "landportal_uris");
-        $ind_path = $module_path . "/model/" . $temporal_indicator . ".json";
-        // Check if the indicator exists
-        if (file_exists($ind_path)) {
-            $ind_data = file_get_contents($ind_path);
-            return json_decode($ind_data, true);
+        $file_path = drupal_get_path("module", "landportal_uris") . "/model/" . $temporal_indicator . ".json";
+
+        if (file_exists($file_path)) {
+            $file_data = file_get_contents($file_path);
+            $indicator_data = json_decode($file_data, true);
+            $indicator_data['entity-id'] = $indicator_id;
+            return $indicator_data;
         } else {
             return false;
         }
--- sites/all/modules/custom/modules/landportal_uris/model/region.php
+++ sites/all/modules/custom/modules/landportal_uris/model/region.php
@@ -4,12 +4,13 @@ class Region {
 	
 	public function get($region_id) {
         $temporal_region = 'region';
-        $module_path = drupal_get_path("module", "landportal_uris");
-        $region_path = $module_path . "/model/" . $temporal_region . ".json";
-        // Check if the indicator exists
-        if (file_exists($region_path)) {
-            $region_data = file_get_contents($region_path);
-            return json_decode($region_data, true);
+        $file_path = drupal_get_path("module", "landportal_uris") . "/model/" . $temporal_region . ".json";
+        
+        if (file_exists($file_path)) {
+            $file_data = file_get_contents($file_path);
+            $region_data = json_decode($file_data, true);
+            $region_data['entity-id'] = $region_id;
+            return $region_data;
         } else {
             return false;
         }
