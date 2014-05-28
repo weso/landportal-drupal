7abc8a884a614380e47c9efccbabde3118d5f5e4
belaustegui <cristian.alvb@gmail.com>
2014-05-28 16:28:46 +0200

    Close #51

---
+++ sites/all/themes/book/comment.tpl.php
@@ -0,0 +1,82 @@
+<?php
+/**
+* @file
+* Book theme implementation for comments.
+*
+* Available variables:
+* - $author: Comment author. Can be link or plain text.
+* - $content: An array of comment items. Use render($content) to print them all, or
+*     print a subset such as render($content['field_example']). Use
+*     hide($content['field_example']) to temporarily suppress the printing of a
+*     given element.
+* - $created: Formatted date and time for when the comment was created.
+*     Preprocess functions can reformat it by calling format_date() with the
+*     desired parameters on the $comment->created variable.
+* - $changed: Formatted date and time for when the comment was last changed.
+*     Preprocess functions can reformat it by calling format_date() with the
+*     desired parameters on the $comment->changed variable.
+* - $new: New comment marker.
+* - $permalink: Comment permalink.
+* - $submitted: Submission information created from $author and $created during
+*     template_preprocess_comment().
+* - $picture: Authors picture.
+* - $signature: Authors signature.
+* - $status: Comment status. Possible values are:
+*     comment-unpublished, comment-published or comment-preview.
+* - $title: Linked title.
+* - $classes: String of classes that can be used to style contextually through
+*     CSS. It can be manipulated through the variable $classes_array from
+*     preprocess functions. The default values can be one or more of the following:
+* - comment: The current template type, i.e., "theming hook".
+* - comment-by-anonymous: Comment by an unregistered user.
+* - comment-by-node-author: Comment by the author of the parent node.
+* - comment-preview: When previewing a new or edited comment.
+*     The following applies only to viewers who are registered users:
+* - comment-unpublished: An unpublished comment visible only to administrators.
+* - comment-by-viewer: Comment by the user currently viewing the page.
+* - comment-new: New comment since last the visit.
+* - $title_prefix (array): An array containing additional output populated by
+*     modules, intended to be displayed in front of the main title tag that
+*     appears in the template.
+* - $title_suffix (array): An array containing additional output populated by
+*     modules, intended to be displayed after the main title tag that appears in
+*     the template.
+*
+* These two variables are provided for context:
+* - $comment: Full comment object.
+* - $node: Node object the comments are attached to.
+*
+* Other variables:
+* - $classes_array: Array of html class attribute values. It is flattened
+* into a string within the variable $classes.
+*
+* @see template_preprocess()
+* @see template_preprocess_comment()
+* @see template_process()
+* @see theme_comment()
+*
+* @ingroup themeable
+*/
+?>
+<?php include_once("template-loader.php");
+  $labels = get_labels($application_data['languages']);
+?>
+<div class="row">
+  <div class="col-sm-12">
+    <!-- Comment title -->
+    <h4><?php echo $title; ?></h4>
+    <!-- Comment author -->
+    <div class="comment-author">
+      <?php echo $labels["written_by"], " ", $author; ?>
+      <?php echo $labels["on"], " ", date($labels["date_format"], $comment->created); ?>
+    </div>
+    <!-- Comment body -->
+    <p><?php echo $comment->comment_body["und"]["0"]["value"]; ?></p>
+    <!-- Comment links -->
+      <div class="row comment-links">
+        <div class="col-sm-12">
+          <?php print render($content["links"]); ?>
+        </div>
+      </div>
+  </div>
+</div>
