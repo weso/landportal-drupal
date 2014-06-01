2e7f80be103aeaf3c3eba15e0cd8bc6a78bf4291
belaustegui <cristian.alvb@gmail.com>
2014-06-01 13:16:39 +0200

    Close #65

--- sites/all/modules/custom/modules/landportal_uris/model/datasource.php
+++ sites/all/modules/custom/modules/landportal_uris/model/datasource.php
@@ -19,7 +19,7 @@ class Datasource {
 
     $database = new DataBaseHelper();
     $connection = $database->open();
-    $organization = $database->query($connection, "organization", array($dat_id));
+    $organization = $database->query($connection, "organization", array($lang, $dat_id));
     if (!$organization):
       drupal_goto("e404");
     endif;
@@ -57,7 +57,7 @@ class Datasource {
       "id" => $data[0]["id"],
       "name" => utf8_encode($data[0]["name"]),
       "url" => $data[0]["url"],
-      "description" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      "description" => utf8_encode($data[0]["description"]),
     );
   }
 
