8fe51d2bbf4ad1670e65d0921ee17e10596f0dca
belaustegui <cristian.alvb@gmail.com>
2014-06-03 10:53:42 +0200

    Fix observations_by_region_average AJAX

--- sites/all/modules/custom/modules/landportal_uris/ajax/observations_by_region_average.php
+++ sites/all/modules/custom/modules/landportal_uris/ajax/observations_by_region_average.php
@@ -4,43 +4,55 @@ include_once("../model/database.php");
 $region1 = $_GET["region1"];
 $region2 = isset($_GET["region2"]) ? $_GET["region2"] : "";
 $indicator = $_GET["indicator"];
-
 $language = $_GET["language"];
 
-$cached = get_from_cache($region1, $region2, $indicator);
-if ($cached !== null)
-  return $cached;
-
-$database = new DataBaseHelper();
-$connection = $database->open();
-
-$regionFilter = $region1 == 1 ? "" : "AND regions.un_code = $region1";
-$averages = $database->query($connection, "observations_by_region_avg", array($regionFilter, $indicator));
-$result1 = compose_data($averages);
-
-$reg1 = $database->query($connection, "continent_name", array($language, $region1));
-$region1Name = $reg1[0]["name"];
-
-$result2 = NULL;
-$region2Name = "";
-
-if ($region2 != "") {
-  $regionFilter = $region2 == 1 ? "" : "AND regions.un_code = $region2";
-  $averages = $database->query($connection, "observations_by_region_avg", array($regionFilter, $indicator));
-  $result2 = compose_data($averages);
-
-  $reg2 = $database->query($connection, "continent_name", array($language, $region2));
-  $region2Name = $reg2[0]["name"];
-}
-
-$database->close($connection);
-
-//if (function_exists("apc_store"))
-//  apc_store(generate_cache_key($region, $indicator), $result);
-
 header('Content-Type: application/json');
+echo observations_by_region_average($region1, $region2, $indicator, $language);
 
-echo json_encode(mergeRegions($region1, $region1Name, $result1, $region2, $region2Name, $result2));
+
+/**
+ * Calculate the observations average for a certain region or regions and an
+ * indicator.
+ * @param $region1 UN_CODE for the region to calculate is average.
+ * @param $region2 UN_CODE for the other region to calculate is average.  This
+ *    parameter may be empty.
+ * @param $indicator ID of the indicator for use in the comparison.
+ * @param $language Language to show the regions name.
+ *
+ * @return Json array with two objects 'series' and 'times'.  'Series' contains
+ *    the regions ID, name and values.  'Times' contains the years used for
+ *    the compairson.
+ */
+function observations_by_region_average($region1, $region2, $indicator, $language) {
+    $cached = get_from_cache($region1, $region2, $indicator, $language);
+    if ($cached !== null) {
+        return $cached;
+    }
+    $database = new DataBaseHelper();
+    $connection = $database->open();
+
+    $regionFilter = $region1 == 1 ? "" : "AND regions.un_code = $region1";
+    $averages = $database->query($connection, "observations_by_region_avg", array($regionFilter, $indicator));
+    $result1 = compose_data($averages);
+    $reg1 = $database->query($connection, "continent_name", array($language, $region1));
+    $region1Name = $reg1[0]["name"];
+
+    $result2 = NULL;
+    $region2Name = "";
+    if ($region2 != "") {
+      $regionFilter = $region2 == 1 ? "" : "AND regions.un_code = $region2";
+      $averages = $database->query($connection, "observations_by_region_avg", array($regionFilter, $indicator));
+      $result2 = compose_data($averages);
+      $reg2 = $database->query($connection, "continent_name", array($language, $region2));
+      $region2Name = $reg2[0]["name"];
+    }
+    $database->close($connection);
+    $json_result = json_encode(mergeRegions($region1, $region1Name, $result1, $region2, $region2Name, $result2));
+    if (function_exists("apc_store")) {
+      apc_store(generate_cache_key($region1, $region2, $indicator, $language), $json_result);
+    }
+    return $json_result;
+}
 
 function mergeRegions($region1, $region1Name, $regionData1, $region2, $region2Name, $regionData2) {
   $times = $regionData2 == NULL ? $regionData1["times"] :
@@ -117,6 +129,11 @@ function get_from_cache($region1, $region2, $indicator) {
  * Generates a unique key to store the result into the cache.
  * @return md5 hash to use as cache-key
  */
-function generate_cache_key($region1, $region2, $indicator) {
-  return hash('md5', "observations_by_region_avg" . $region1 . $region2 . $indicator);
+function generate_cache_key($region1, $region2, $indicator, $language) {
+    return hash('md5', "observations_by_region_avg"
+        . $region1
+        . $region2
+        . $indicator
+        . $language
+    );
 }
