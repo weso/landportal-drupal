0ce64c1b3c8896a196fb79cebb5c4137668c5bf5
belaustegui <cristian.alvb@gmail.com>
2014-06-18 11:14:39 +0200

    Make the home view configurable.

--- sites/all/modules/custom/modules/landportal_uris/model/home.php
+++ sites/all/modules/custom/modules/landportal_uris/model/home.php
@@ -1,7 +1,25 @@
 <?php
 
+
 class Home {
     public function get($options) {
-        return "";
+    	$language = $options->language;
+        $config_file = file_get_contents(dirname(__FILE__) .'/../home.json');
+        $config = (array)json_decode($config_file, true);
+
+        $count = 0;
+        $result = array();
+        foreach ($config as $item) {
+        	$count++;
+        	array_push($result, array(
+        		"image" => $item["image"],
+        		"text" => $item["text"][$language]
+    		));
+    		if ($count == 4) {
+    			break;
+    		}
+        }
+
+        return array('carousel' => $result);
     }
 }
