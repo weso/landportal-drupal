944eda5ad9b2ffd8bc476dcdd22a29507f8caffa
castrofernandez <castrofernandez@gmail.com>
2014-10-24 10:04:49 +0200

    Increase time elapse

--- sites/all/themes/book/js/controller/country.js
+++ sites/all/themes/book/js/controller/country.js
@@ -365,7 +365,7 @@ function loadComparingTimeline(parameters) {
 					parameters: String.format("country1={0}&country2={1}&indicator={2}&language={3}",
 																		country1, country2, indicator, languageCode),
 				});
-			}, count * 100);
+			}, count * 300);
 		})(indicator);
 		
 		count++;
